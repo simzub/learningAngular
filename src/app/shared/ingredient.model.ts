fb43b3e85faad39786e451fe145ade3fa1838bd6
simzub <78310955+simzub@users.noreply.github.com>
2023-07-17 09:16:22 +0300

    Add shopping components

--- src/app/shared/ingredient.model.ts
+++ src/app/shared/ingredient.model.ts
@@ -1,9 +1,6 @@
 export class Ingredient {
-  public name: string;
-  public amount: number;
-
-  constructor(name: string, amount: number) {
-    this.name = name;
-    this.amount = amount;
-  }
+  constructor(
+    public name: string,
+    public amount: number
+  ) {}
 }
