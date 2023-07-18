a080e95025fd887bcc5b2bbf34e81e76ea8a13b6
simzub <78310955+simzub@users.noreply.github.com>
2023-07-18 14:25:07 +0300

    add passing data with event and property binding and add feature to let user add ingredients to shoppingList

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,20 +7,26 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe(
       'A Test Recipe',
       'This is simply a test',
-      'https://cdn.tasteatlas.com/images/recipes/4be38cdcfda14aca84bf062e5c304a8b.jpg',
+      'https://cdn.tasteatlas.com/images/recipes/4be38cdcfda14aca84bf062e5c304a8b.jpg'
     ),
     new Recipe(
-      'A Test Recipe',
+      'Another Test Recipe',
       'This is simply a test',
-      'https://cdn.tasteatlas.com/images/recipes/4be38cdcfda14aca84bf062e5c304a8b.jpg',
+      'https://cdn.tasteatlas.com/images/recipes/4be38cdcfda14aca84bf062e5c304a8b.jpg'
     ),
   ];
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
