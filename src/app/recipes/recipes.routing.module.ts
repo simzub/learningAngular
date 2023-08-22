d9c85513640a686f1dd0b596338e0038170d8d94
simzub <78310955+simzub@users.noreply.github.com>
2023-08-22 12:32:37 +0300

    add lazy loading

--- src/app/recipes/recipes.routing.module.ts
+++ src/app/recipes/recipes.routing.module.ts
@@ -9,7 +9,7 @@ import { RecipesResolverService } from './recipe-resolver.service';
 
 const routes: Routes = [
   {
-    path: 'recipes',
+    path: '',
     component: RecipesComponent,
     canActivate: [AuthGuard],
     children: [
